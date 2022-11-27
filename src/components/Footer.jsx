b6830ba98460f2b2950fd4de7428437821c9a333
Ranjeet Soni <ranjeetsoni1996@gmail.com>
2022-11-27 22:41:01 +0530

    portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillYoutube,
+  AiOutlineArrowUp,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <img
+          src={"https://avatars.githubusercontent.com/u/25058652"}
+          alt="Founder"
+        />
+
+        <h2>Ranjeet kumar</h2>
+        <p>Motivation is temporary, but discipline last forever.</p>
+      </div>
+
+      <aside>
+        <h2>Social Media</h2>
+
+        <article>
+          <a href="https://www.linkedin.com/in/ranjeet-soni/" target={"blank"}>
+            <AiFillLinkedin />
+          </a>
+          <a href="https://www.instagram.com/ranjeet8996/" target={"blank"}>
+            <AiFillInstagram />
+          </a>
+          <a href="https://github.com/ranjeet1996" target={"blank"}>
+            <AiFillGithub />
+          </a>
+        </article>
+      </aside>
+      <a href="#home">
+        <AiOutlineArrowUp />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
