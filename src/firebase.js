0d545282fa4f1f26fa38a6f808ef449720c52d81
Ranjeet Soni <ranjeetsoni1996@gmail.com>
2023-05-07 11:23:42 +0530

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,4 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
 
-// // const firebaseConfig = {
-// //   apiKey: "AIzaSyDYAW9F1kP29j38dfZuZxK4lppUkSLD25c",
-// //   authDomain: "react-portfolio-abhishek.firebaseapp.com",
-// //   projectId: "react-portfolio-abhishek",
-// //   storageBucket: "react-portfolio-abhishek.appspot.com",
-// //   messagingSenderId: "278164302078",
-// //   appId: "1:278164302078:web:797fe5960bb12b5fca1daf",
-// // };
-
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDbiMd01z5rOiBxyz5J06AbiSRPh8nYs-o",
-//   authDomain: "portfolio-2a8e7.firebaseapp.com",
-//   projectId: "portfolio-2a8e7",
-//   storageBucket: "portfolio-2a8e7.appspot.com",
-//   messagingSenderId: "499778744343",
-//   appId: "1:499778744343:web:29a3139a1364553ea82c52",
-
-// };
-
-
-// export const app = initializeApp(firebaseConfig);
-// export const db = getFirestore();
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
